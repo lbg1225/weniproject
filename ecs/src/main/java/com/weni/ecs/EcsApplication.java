37c41349c07eadd090d5535c34cc517111673335
Lee Byounggwan <lbg1225@naver.com>
2022-03-27 21:49:06 +0900

    second

--- ecs/src/main/java/com/weni/ecs/EcsApplication.java
+++ ecs/src/main/java/com/weni/ecs/EcsApplication.java
@@ -5,11 +5,13 @@ package com.weni.ecs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // import org.springframework.cloud.stream.annotation.EnableBinding;
 // import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // gradlew -> gradlew jar
 @SpringBootApplication
+@EnableJpaAuditing     // JPA Auditing을 활성화 하기 위한 어노테이션
 // @EnableBinding(KafkaProcessor.class)
 // @EnableFeignClients
 public class EcsApplication {
--- sample/src/main/java/com/sample/SampleApplication.java
+++ sample/src/main/java/com/sample/SampleApplication.java
@@ -1,12 +1,16 @@
 package com.sample;
 
+import com.sample.config.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing     // JPA Auditing을 활성화 하기 위한 어노테이션
 @SpringBootApplication
+// @EnableBinding(KafkaProcessor.class)
 public class SampleApplication {
+	protected static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
